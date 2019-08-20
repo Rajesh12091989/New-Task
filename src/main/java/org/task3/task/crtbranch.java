2628b799aec3520e494dd452d7ad013e8d92326a
Rajesh12091989 <testerrajesh1990@gmail.com>
2019-08-19 22:10:58 -0700

    new task

---
+++ src/main/java/org/task3/task/crtbranch.java
@@ -0,0 +1,13 @@
+package org.task3.task;
+
+public class crtbranch {
+public static void main(String[] args) {
+	int a=10;
+	if (a < 10){
+		System.out.println("Valid");
+	}
+	else{
+		System.out.println("Invalid");
+	}
+}
+}
